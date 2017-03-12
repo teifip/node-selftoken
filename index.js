49b74c458ca8e3efdf4c33786155235e2981f6a2
teifip <teifip@gmail.com>
2017-03-11 19:38:00 -0800

    Added handling of null token
    Minor code clean-up

--- index.js
+++ index.js
@@ -16,30 +16,34 @@ module.exports = function(options) {
   this.pwd = crypto.randomBytes(32);
 
   this.generate = function(payload, callback) {
-    if (typeof payload !== 'string') {
+    if (!payload || typeof payload !== 'string') {
       callback(new Error('Input data is not a string'));
-    } else {
-      if (this.lifecycle > 0) {
-        var data = JSON.stringify({p: payload, e: Date.now() + this.lifecycle});
-      } else {
-        var data = JSON.stringify({p: payload});
-      }
-      var salt = crypto.randomBytes(16);
-      crypto.pbkdf2(this.pwd, salt, this.iter, 16, 'sha256', (error, key) => {
-        if (error) {
-          callback(error);
-        } else {
-          var hash = crypto.createHmac('sha256', key);
-          hash.update(data);
-          var mac = Buffer.concat([salt, hash.digest().slice(0, this.len)]);
-          var dataB64u = b64tob64url(Buffer.from(data).toString('base64'));
-          callback(null, dataB64u + '.' + b64tob64url(mac.toString('base64')));
-        }
-      });
+      return;
     }
+    if (this.lifecycle > 0) {
+      var data = JSON.stringify({p: payload, e: Date.now() + this.lifecycle});
+    } else {
+      var data = JSON.stringify({p: payload});
+    }
+    var salt = crypto.randomBytes(16);
+    crypto.pbkdf2(this.pwd, salt, this.iter, 16, 'sha256', (error, key) => {
+      if (error) {
+        callback(error);
+        return;
+      }
+      var hash = crypto.createHmac('sha256', key);
+      hash.update(data);
+      var mac = Buffer.concat([salt, hash.digest().slice(0, this.len)]);
+      var dataB64u = b64tob64url(Buffer.from(data).toString('base64'));
+      callback(null, dataB64u + '.' + b64tob64url(mac.toString('base64')));
+    });
   }
 
   this.verify = function(token, callback) {
+    if (!token || typeof token !== 'string') {
+      callback(new Error('Invalid token'));
+      return;
+    }
     var tokenComponents = token.split('.');
     var data = Buffer.from(tokenComponents[0], 'base64');
     var mac = Buffer.from(tokenComponents[1] || '', 'base64');
@@ -47,23 +51,23 @@ module.exports = function(options) {
     crypto.pbkdf2(this.pwd, salt, this.iter, 16, 'sha256', (error, key) => {
       if (error) {
         callback(error);
-      } else {
-        var hash = crypto.createHmac('sha256', key);
-        hash.update(data);
-        if (mac.slice(16).equals(hash.digest().slice(0, this.len))) {
-          safeParse(data.toString(), (error, parsedData) => {
-            if (error) {
-              callback(error);
-            } else if (parsedData.e && Date.now() > parsedData.e) {
-              callback(new Error('Expired token'));
-            } else {
-              callback(null, parsedData.p);
-            }
-          });
-        } else {
-          callback(new Error('Invalid token'));
-        }
+        return;
       }
+      var hash = crypto.createHmac('sha256', key);
+      hash.update(data);
+      if (!mac.slice(16).equals(hash.digest().slice(0, this.len))) {
+        callback(new Error('Invalid token'));
+        return;
+      }
+      safeParse(data.toString(), (error, parsedData) => {
+        if (error) {
+          callback(error);
+        } else if (parsedData.e && Date.now() > parsedData.e) {
+          callback(new Error('Expired token'));
+        } else {
+          callback(null, parsedData.p);
+        }
+      });
     });
   }
 }
